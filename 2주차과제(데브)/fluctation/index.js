a0eacc557dd459c524a653f0ed3f39093038a5f8
JiminLee <201910825>
2023-09-27 13:35:06 +0900

    2주차

---
+++ 2주차과제(데브)/fluctation/index.js
@@ -0,0 +1,9 @@
+
+let num = document.querySelector("#num");
+
+function inclick(){
+    num.innerHTML = parseInt(num.innerHTML) + 1;
+}
+function declick(){
+    num.innerHTML = parseInt(num.innerHTML) - 1;
+}
