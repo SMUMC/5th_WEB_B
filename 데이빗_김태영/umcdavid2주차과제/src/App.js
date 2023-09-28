b0aeb5fad65309c37e5f8b62dd2a44e8606c1f0a
kimtaeyoung <trust00005@naver.com>
2023-09-28 22:04:28 +0900

    2주차 과제 in umcdavid

---
+++ 데이빗_김태영/umcdavid2주차과제/src/App.js
@@ -0,0 +1,16 @@
+
+import './App.css';
+
+import Join from './Join';
+
+function App() {
+  return (
+    <div className="App">
+      {/* <Counter /> */}
+      {/* <Modal /> */}
+      <Join />
+    </div>
+  );
+}
+
+export default App;
