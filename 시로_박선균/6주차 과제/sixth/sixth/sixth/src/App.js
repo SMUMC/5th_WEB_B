25420d3404682f4277a8d37249ea1f1fea7d4878
ParkSeonGyun <qkrtjsrbs315@gmail.com>
2023-11-06 18:27:43 +0900

    6주차 최종완성

---
+++ 시로_박선균/6주차 과제/sixth/sixth/sixth/src/App.js
@@ -0,0 +1,15 @@
+import './App.css';
+import Nav from './components/nav/nav'
+import CartContainer from './components/cart/cartContainer'
+
+
+function App() {
+  return (
+      <div className='App'>
+        <Nav/>
+        <CartContainer/>
+      </div>
+  );
+}
+
+export default App;
