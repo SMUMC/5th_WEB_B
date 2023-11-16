3612ced6b4318eeec56656ffd66b3ba58a9ba38a
DOTORIDANG <hajisu2305@gmail.com>
2023-11-16 17:05:42 +0900

    8th_mission

---
+++ 도톨이_하지수/8th_mission_final/8th_mission1_web/src/components/Login.style.jsx
@@ -0,0 +1,25 @@
+import styled from "styled-components";
+
+export const StyledInput = styled.input`
+  width: 500px;
+  height: 50px;
+  margin: 12px;
+  border-radius: 10px;
+`;
+
+export const StyledButton = styled.button`
+  width: 100px;
+  height: 50px;
+  margin: 12px;
+  border-radius: 10px;
+`;
+
+export const StyledWeather = styled.div`
+  width: 200px;
+  height: 150px;
+  margin: 12px;
+  background-color: #e7e7e7;
+  padding-top: 8px;
+  text-align: center;
+  border-radius: 10px;
+`;
