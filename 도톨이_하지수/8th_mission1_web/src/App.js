d0c4a8688731db21bab9ac16252dfc1e34fb627e
DOTORIDANG <hajisu2305@gmail.com>
2023-11-16 17:18:48 +0900

    8th_mission1

---
+++ 도톨이_하지수/8th_mission1_web/src/App.js
@@ -0,0 +1,7 @@
+import Login from "./components/LoginComponent";
+
+function App() {
+  return <Login />;
+}
+
+export default App;
---
+++ 도톨이_하지수/8th_mission1_web/src/redux/store.js
@@ -0,0 +1,8 @@
+import { configureStore } from "@reduxjs/toolkit";
+import userSlice from "./userSlice.js";
+
+export default configureStore({
+  reducer: {
+    user: userSlice,
+  },
+});
